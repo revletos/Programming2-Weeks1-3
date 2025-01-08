79ebba4fcab37716c17dff1a762f94865bd03717
Whale <revletos@gmail.com>
2025-01-08 11:43:08 -0500

    Made Square Bounce from Edge to Edge X way

--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -16,6 +16,11 @@ public class SquareScript : MonoBehaviour
     {
         Vector2 pos = transform.position;
         pos.x += speed;
+        Vector2 squareInScreenSpace = Camera.main.WorldToScreenPoint(pos);
+
+        if(squareInScreenSpace.x < 0 || squareInScreenSpace.x > Screen.width)
+        { speed = speed * -1;
+        }
         transform.position = pos;
         //transform.position.x += 1;
     }
